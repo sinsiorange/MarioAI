aea3634ddcb7f967c22f57a84fc9bafcb0323c81
sinsiorange <sinsiorange37@gmail.com>
2018-09-14 17:25:31 +0900

    add MyAgent

---
+++ src/ch/idsia/agents/controllers/MyAgent.java
@@ -0,0 +1,23 @@
+package ch.idsia.agents.controllers;
+
+import ch.idsia.agents.Agent;
+import ch.idsia.benchmark.mario.engine.sprites.Mario;
+import ch.idsia.benchmark.mario.environments.Environment;
+
+public class MyAgent  extends BasicMarioAIAgent implements Agent
+{
+	public MyAgent()
+	{
+		super("MyAgent");
+	}
+	
+	public boolean[] getAction()
+	{
+	    
+	}
+	
+	public void reset()
+	{
+	    
+	}
+}
